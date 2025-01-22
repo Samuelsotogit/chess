cf06bbe8d5f39390c4d3fd9c275c6f9de2308fa9
samuel <samuelsotointernational@gmail.com>
2025-01-22 01:36:23 -0700

    Passed the rook capture enemy test

--- shared/src/main/java/chess/RookMove.java
+++ shared/src/main/java/chess/RookMove.java
@@ -14,27 +14,47 @@ public class RookMove implements PieceMoveCalculator{
         }
         if (piece.getPieceType() == ChessPiece.PieceType.ROOK) {
             ChessPiece rook = piece;
-            // If there are no pieces ahead, those are valid moves.
-            int i = 1;
-            ChessPosition tempPosition = new ChessPosition(position.getRow()+i, position.getColumn());
-            while (isInBounds(tempPosition)) {
-                ChessPiece nextPiece = board.getPiece(tempPosition);
-                if (nextPiece == null) {
-                    moves.add(new ChessMove(position, tempPosition, null));
-                    i++;
-                }
-                //As soon as there is a piece in the next column, that rook can be moved to that position as a valid move (taking).
-                else if (nextPiece.getTeamColor() != rook.getTeamColor()) {
-                    moves.add(new ChessMove(position, tempPosition, null));
-                    //Stop adding moves if capturing a piece.
-                    return moves;
-                }
-                //Update square being checked as possible move.
-                tempPosition = new ChessPosition(position.getRow() + i, position.getColumn());
 
+            // Use direction vectors to iterate through all possible moves.
+            int[][] directions = {
+                    {1, 0},
+                    {-1, 0},
+                    {0, 1},
+                    {0, -1}
+            };
+
+            // Iterate through all possible directions.
+            for (int[] direction : directions) {
+                int rowIncrement = direction[0];
+                int columnIncrement = direction[1];
+                int i = 1;
+
+                ChessPosition nextPosition = new ChessPosition(position.getRow() + i * rowIncrement, position.getColumn() + i * columnIncrement);
+
+                while (isInBounds(nextPosition)) {
+                    ChessPiece nextPiece = board.getPiece(nextPosition);
+                    // If there are no pieces ahead, those are valid moves.
+                    if (nextPiece == null) {
+                        moves.add(new ChessMove(position, nextPosition, null));
+                    }
+                    //As soon as there is a piece in the next column, that rook can be moved to that position as a valid move (taking).
+                    else if (nextPiece.getTeamColor() != rook.getTeamColor()) {
+                        moves.add(new ChessMove(position, nextPosition, null));
+                        //Stop adding moves if capturing a piece.
+                        break;
+                    }
+                    else if (nextPiece.getTeamColor() == rook.getTeamColor()) {
+                        //Stop adding moves if blocked by a piece of the same color.
+                        break;
+                    }
+                    //Update square being checked as possible move.
+                    i++;
+                    nextPosition = new ChessPosition(position.getRow() + i * rowIncrement, position.getColumn() + i * columnIncrement);
+                }
             }
         }
         return moves;
+
     }
     private boolean isInBounds(ChessPosition myPosition) {
         return myPosition.getRow() <= 8 && myPosition.getColumn() <= 8 && myPosition.getColumn() >= 1 && myPosition.getRow() >= 1;
