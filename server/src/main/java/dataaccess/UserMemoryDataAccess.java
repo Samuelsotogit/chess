a0854fb1926c6111467070dd62e54788cbd192c7
samuel <samuelsotointernational@gmail.com>
2025-03-03 17:36:01 -0700

    Implemented basic UserDAO methods

--- server/src/main/java/dataaccess/UserMemoryDataAccess.java
+++ server/src/main/java/dataaccess/UserMemoryDataAccess.java
@@ -1,8 +1,28 @@
 package dataaccess;
+import model.AuthData;
+import model.UserData;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class UserMemoryDataAccess implements UserDAO {
+    private final HashMap<String, UserData> users = new HashMap<>();
+
     @Override
-    public Object getUser(Object user) throws DataAccessException {
-        return user;
+    public UserData getUser(String username) throws DataAccessException {
+        if (users.containsKey(username)) {
+            return users.get(username);
+        }
+        return null;
+    }
+
+    @Override
+    public void createUser(UserData data) throws DataAccessException {
+        UserData newUser = new UserData(data.username(), data.password(), data.email());
+        users.put(data.username(), newUser);
+    }
+
+    @Override
+    public void deleteUsers() {
+        users.clear();
     }
 }
