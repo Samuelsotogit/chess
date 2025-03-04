92ab82e39c0529a8b762013c97893dbf8c2d7862
samuel <samuelsotointernational@gmail.com>
2025-03-03 17:34:58 -0700

    Refactored server class to be more modular

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,27 @@
 package server;
-
+import dataaccess.AuthMemoryDataAccess;
+import dataaccess.UserMemoryDataAccess;
+import server.handlers.DeleteHandler;
+import server.handlers.LoginHandler;
+import server.handlers.RegistrationHandler;
+import service.UserService;
 import spark.*;
 
 public class Server {
+
+    UserMemoryDataAccess userDAO = new UserMemoryDataAccess();
+    AuthMemoryDataAccess authDAO = new AuthMemoryDataAccess();
+    UserService userService;
+    LoginHandler loginHandler;
+    RegistrationHandler registrationHandler;
+    DeleteHandler deleteHandler;
+
+    public Server() {
+        this.userService = new UserService(userDAO, authDAO);
+        this.loginHandler = new LoginHandler(userService);
+        this.registrationHandler = new RegistrationHandler(userService);
+        this.deleteHandler = new DeleteHandler(userService);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,7 +29,11 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.post("/user", registrationHandler::registerUser);
+        Spark.delete("/db", deleteHandler::clearDatabase);
+        Spark.post("/session", loginHandler::loginUser);
+        Spark.delete("session", loginHandler::logout);
+//        Spark.get("/game", );
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
@@ -22,4 +45,5 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
 }
