226be5dd2117c39df4da9164dffcc1d7096223e8
samuel <samuelsotointernational@gmail.com>
2025-03-30 16:49:40 -0600

    Set up my serverFacade with required methods.

---
+++ client/src/main/java/server/ServerFacade.java
@@ -0,0 +1,121 @@
+package server;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.AuthData;
+import data.transfer.objects.*;
+import java.io.*;
+import java.net.*;
+import java.util.Arrays;
+import shared.ResponseException;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public String register(String username, String password, String email) throws ResponseException {
+        try {
+            var path = "/user";
+            var request = new RegisterRequest(username, password, email);
+            return this.makeRequest("POST", path, request, String.class);
+        } catch (ResponseException e) {
+            return e.getMessage();
+        }
+    }
+
+    public String login(String username, String password) throws ResponseException {
+        var path = "/session";
+        var request = new LoginRequest(username, password);
+        return this.makeRequest("POST", path, request, String.class);
+    }
+
+    public void logout(String username, String authToken) throws ResponseException {
+        var path = "/session";
+        var request = new AuthData(username, authToken);
+        this.makeRequest("DELETE", path, request.authToken(), null);
+    }
+
+    public Object listGames(String username, String authToken) throws ResponseException {
+        var path = "/game";
+        var request = new AuthData(username, authToken);
+        return this.makeRequest("GET", path, request.authToken(), Object.class);
+    }
+
+    public Integer createGames(String username, String authToken) throws ResponseException {
+        var path = "/game";
+        var request = new AuthData(username, authToken);
+        return this.makeRequest("POST", path, request.authToken(), Integer.class);
+    }
+
+    public void joinGame(String authToken, String playerColor, String gameID) throws ResponseException {
+        var path = "/game";
+        ChessGame.TeamColor color = playerColor.equals("white") ? ChessGame.TeamColor.WHITE : ChessGame.TeamColor.BLACK;
+        Integer id = Integer.parseInt(gameID);
+        var request = new JoinGameRequest(authToken, color, id);
+        this.makeRequest("PUT", path, request, null);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
