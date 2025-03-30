e24550c958dcee6f101b0fee3e3d418f9a602836
samuel <samuelsotointernational@gmail.com>
2025-03-30 16:50:52 -0600

    Set up my repl file

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,38 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println(WHITE_KING + "Welcome to the best chess game ever. Sign in to start.");
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(result.toUpperCase());
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + SET_TEXT_UNDERLINE + SET_TEXT_COLOR_GREEN +
+                "[LOGGED_OUT]" + RESET_TEXT_UNDERLINE + " >>> " + RESET_TEXT_COLOR);
+    }
 }
